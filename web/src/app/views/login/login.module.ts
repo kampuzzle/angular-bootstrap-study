37ab608e4e295f8596b7a798cb9284bdb3759357
kampuzzle <paulabremenkamp@gmail.com>
2024-07-18 15:15:33 -0300

    modular

---
+++ web/src/app/views/login/login.module.ts
@@ -0,0 +1,24 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  exports: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ]
+})
+export class LoginModule { }
